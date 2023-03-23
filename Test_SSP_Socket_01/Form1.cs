7cfb86ecbb41203ed7e709ec50b03cc4f6e46ddd
sjlon <sjlon@SSP-DESK-RAVI>
2023-03-23 15:25:48 +0900

    초기 커밋.

---
+++ Test_SSP_Socket_01/Form1.cs
@@ -0,0 +1,28 @@
+﻿//using MetroFramework.Forms;
+using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_SSP_Socket_01
+{
+    //public partial class Form1 : MetroForm
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
